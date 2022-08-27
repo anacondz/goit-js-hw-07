6176a84f6ff9b5a4e637c338616dffe9e90760a0
anacondz <97612929+anacondz@users.noreply.github.com>
2022-08-27 17:11:32 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgGalleryItem = document.querySelector('.gallery');
+const imgGallery = createDivs(galleryItems);
 
-console.log(galleryItems);
+imgGalleryItem.insertAdjacentHTML('beforeend', imgGallery);
+
+function createDivs(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return`
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+}
+
+imgGalleryItem.addEventListener('click', bigSizeImg);
+
+function bigSizeImg(event) {
+    event.preventDefault();
+    const imgColor = event.target.classList.contains(`gallery__image`);
+ 
+    if (!imgColor) {
+        return;
+    }
+   
+    const modalEl = event.target.dataset.source;
+     
+    const instance = basicLightbox.create(`<img class="modal__image" src="${modalEl}"/>`);
+    instance.show();
+   
+    window.addEventListener('keydown', escPress);
+    function escPress(event) {
+        const KeyEsc = 'Escape';
+        if (event.code === KeyEsc) {
+            instance.close();
+            window.removeEventListener('keydown', escPress);
+        }
+    }
+}
